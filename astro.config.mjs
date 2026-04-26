a8845dd37c90ee65412bf86c89127fdc17c7adea
Rose <rosalie.musilova@gmail.com>
2026-04-26 13:57:23 +0200

    nastavení GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://tixalim.github.io',
+    base: 'waldorf-semily',
+});
